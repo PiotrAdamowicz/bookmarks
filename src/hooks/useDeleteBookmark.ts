59e4f8b088d3ac351825d5017d7a00aa2ca7db3f
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-08-22 18:34:01 +0200

    feat: :zap: delete functinality

--- src/hooks/useDeleteBookmark.ts
+++ src/hooks/useDeleteBookmark.ts
@@ -1,16 +1,30 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "../api/api";
 import { QUERY_KEYS, URL } from "../constants";
+import type { Bookmark } from "../types";
 
-export function useDeleteBookmark(id: number) {
+// export function useDeleteBookmark() {
+//     const queryClient = useQueryClient();
+
+//     return useMutation({
+//         mutationFn: async (id: number) =>
+//             await api(`${URL.bookmarks}/${id}`, { method: "DELETE" }),
+//     });
+// }
+
+export function useDeleteBookmark() {
     const queryClient = useQueryClient();
 
     return useMutation({
-        mutationFn: (id: number) =>
-            api(URL.bookmarks + id, { method: "DELETE" }),
-        onSuccess: () => {
-            // invalidate cached bookmarks so UI refreshes
-            queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.bookmarks] });
+        mutationFn: async (id: number) =>
+            await api(`${URL.bookmarks}/${id}`, { method: "DELETE" }),
+
+        onSettled: () => {
+            setTimeout(() => {
+                queryClient.invalidateQueries({
+                    queryKey: [QUERY_KEYS.bookmarks],
+                });
+            }, 300);
         },
     });
 }
--- src/routes/BookmarksPage.tsx
+++ src/routes/BookmarksPage.tsx
@@ -1,15 +1,30 @@
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { clearToken } from "../helpers/tokenHelpers";
 import { useQueryBookmarks } from "../hooks/useQueryBookmarks";
-import { URL } from "../constants";
+import { QUERY_KEYS, URL } from "../constants";
+import { useDeleteBookmark } from "../hooks/useDeleteBookmark";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Bookmarks() {
     const { bookmarks, isPending, error } = useQueryBookmarks();
     const navigate = useNavigate();
+    const remove = useDeleteBookmark();
+    const queryClient = useQueryClient();
 
     const onLogout = () => {
         clearToken();
         navigate(URL.home);
+    };
+
+    const removeBookmark = (id: number) => {
+        remove.mutate(id, {
+            onSuccess: () => {
+                // invalidate cached bookmarks so UI refreshes
+                queryClient.invalidateQueries({
+                    queryKey: [QUERY_KEYS.bookmarks],
+                });
+            },
+        });
     };
 
     if (error) {
@@ -22,14 +37,28 @@ export default function Bookmarks() {
         return <p>No bookmarks</p>;
     }
     return (
-        <>
+        <section className="flex flex-col">
             <h1 className="text-4xl text-center">Bookmarks page</h1>
             <ul className="text-center my-4 text-4xl">
                 {bookmarks.map((bookmark) => (
-                    <li key={bookmark.id}>{bookmark.title}</li>
+                    <li key={bookmark.id}>
+                        <h3>{bookmark.title}</h3>
+                        <button
+                            onClick={() => removeBookmark(bookmark.id)}
+                            className="bg-red-600 text-xl p-2 rounded-md cursor-pointer"
+                        >
+                            Remove
+                        </button>
+                    </li>
                 ))}
             </ul>
+            <Link
+                to="/add"
+                className="bg-green-500 text-center inline py-2 px-8 cursor-pointer rounded-md m-2 max-w-md text-white mx-auto font-bold"
+            >
+                Add
+            </Link>
             <button onClick={onLogout}>Logout</button>
-        </>
+        </section>
     );
 }
