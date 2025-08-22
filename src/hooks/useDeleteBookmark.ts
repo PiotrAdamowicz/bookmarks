0f1fdf0a333e7d856931cfe74d9e0ae913e58e1e
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-08-22 19:51:48 +0200

    chore: :wastebasket: code cleanup

--- src/hooks/useDeleteBookmark.ts
+++ src/hooks/useDeleteBookmark.ts
@@ -1,16 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "../api/api";
 import { QUERY_KEYS, URL } from "../constants";
-import type { Bookmark } from "../types";
-
-// export function useDeleteBookmark() {
-//     const queryClient = useQueryClient();
-
-//     return useMutation({
-//         mutationFn: async (id: number) =>
-//             await api(`${URL.bookmarks}/${id}`, { method: "DELETE" }),
-//     });
-// }
 
 export function useDeleteBookmark() {
     const queryClient = useQueryClient();
